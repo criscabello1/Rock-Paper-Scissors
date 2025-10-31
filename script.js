466a229593c05bfee68901a2716de9503e251d02
Cristóbal <cristobalcabello624@gmail.com>
2025-10-31 14:47:56 -0300

    The function getHumanChoise, wich returns a choise according frm a user's promt, was created

--- script.js
+++ script.js
@@ -8,3 +8,15 @@ function getComputerChoise(){
   else return 'scissors'
 }
 
+// Now i have to create a function that request an input from the user.
+// I will do this with a prompt. I'll save the prompt to a variable
+// and depending of the value of this variable the function will return the valid choises.
+
+function getHumanChoise(){
+  const humanChoise = +prompt('Enter a number for your choise: (1 = "rock", 2 = "paper", 3 = "scissors")')
+	if (humanChoise == 1) return 'rock'
+	else if (humanChoise == 2) return 'paper'
+	else if (humanChoise == 3) return 'scissors' 
+
+}
+
